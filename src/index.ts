6f97691b2947c519adb0d57f9c3ae9176fe1f624
Andy Tang <andyt@live.nl>
2017-07-07 13:13:46 +0200

    multiple components are retrieved from a single string

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export function getComponents(documentation: String) {
-  const matches = documentation.match(/^```(.*)```$/g)
+  const matches = documentation.match(/```(.*)(.*|\n+|\r+|\r\n+)(.*)(\n+|\r+|\r\n+|.*)(.*)```/g)
   if (!matches) return []
-  return matches.map(component => component.replace(/```/g, ''))
+  return matches.map(component => component.replace(/(```|\n|\r')/g, ''))
 }
--- test/index.specs.ts
+++ test/index.specs.ts
@@ -4,8 +4,8 @@ import { getComponents } from '../src/index'
 
 
 describe('when retrieving components', () => {
+  let result: Array<String> = []
   describe('when having one code block', () => {
-    let result: Array<String> = []
     before(() => {
       result = getComponents([
         '```',
@@ -16,6 +16,18 @@ describe('when retrieving components', () => {
 
     it('should retrieve my component', () => {
       expect(result).to.deep.equal(['<my-component></my-component>'])
+    })
+  })
+  describe('when having a code block with new lines', () => {
+    before(() => {
+      result = getComponents(`\`\`\`
+          <my-component></my-component>
+        \`\`\``)
+    })
+
+    it('should retrieve my component', () => {
+      const expectation = '          <my-component></my-component>        '
+      expect(result).to.deep.equal([expectation])
     })
   })
 })
