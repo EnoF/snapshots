a7541808865285c99474ba22ca1ce11337686451
Andy Tang <andyt@live.nl>
2017-07-07 16:55:20 +0200

    snapshots are written to a file

--- index.ts
+++ index.ts
@@ -1,18 +1,33 @@
 import { getComponents } from './src/snapshots'
 import * as fs from 'fs'
 
-export default async function snapshots(dir: string) {
+export default async function snapshots(dir: string, outputDir: string) {
+  const shots = await createSnapshots(dir, outputDir)
+  fs.writeFileSync(`${outputDir}/results.snap`, stringifySnapshots(shots), (err) => {
+    if (err) console.error(err)
+  })
+  return shots
+}
+
+function createSnapshots(dir: string, outputDir: string) {
   return new Promise((resolve, reject) => {
     fs.readdir(dir, async (err, subPaths: Array<string>) => {
       if (err) return reject(err)
       return resolve(await Promise
-        .all(subPaths.map(subPath => resolveComponents(`${dir}/${subPath}`))))
+        .all(subPaths.map(subPath => resolveComponents(`${dir}/${subPath}`, outputDir))))
     })
   })
 }
 
-async function resolveComponents(path: string) {
-  if (await isDir(path)) return snapshots(path)
+function stringifySnapshots(shots: any) {
+  return '[' + shots.map((shot: any) => {
+    if (Array.isArray(shot)) return stringifySnapshots(shot)
+    return `"${shot}"`
+  }).join(',') + ']'
+}
+
+async function resolveComponents(path: string, outputDir: string) {
+  if (await isDir(path)) return snapshots(path, outputDir)
   return getComponents(await getFile(path))
 }
 
--- test/index.specs.ts
+++ test/index.specs.ts
@@ -1,11 +1,12 @@
 import { expect } from 'chai'
 import 'mocha'
 import snapshots from '../index'
+import * as fs from 'fs'
 
 describe('when snapshotting documentation', () => {
   let result: any = null
   before(async () => {
-    return result = await snapshots(`${__dirname}/fixtures`)
+    return result = await snapshots(`${__dirname}/fixtures`, __dirname)
   })
 
   it('should have the root component', () => {
@@ -18,6 +19,15 @@ describe('when snapshotting documentation', () => {
 
   it('should have the my component', () => {
     expect(recursiveSearch('<my-component>', result)).to.equal(true)
+  })
+
+  it('should have saved the snapshots', (done) => {
+    fs.readFile(`${__dirname}/results.snap`, 'utf8', (err, content) => {
+      if (err) done(err)
+      console.log(content)
+      expect(JSON.parse(content)).to.deep.equal(result)
+      done()
+    })
   })
 })
 
